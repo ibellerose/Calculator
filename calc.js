e70b04d70b53864dec2076323709d7b0fdd40bd9
Ian Bellerose <ianbellerose@gmail.com>
2022-01-10 23:27:38 -0500

    align mathStatement for overflow

--- calc.js
+++ calc.js
@@ -11,119 +11,133 @@ class ButtonClass extends React.Component{
     }
 
     addToScreen = (btnPushed) => {
-        if(btnPushed == "AC"){
-            this.setState({calcVariable: '0', mathStatement: ''})
+        if((this.state.calcVariable.length >= 12 && btnPushed >=0)){
         }
-        else if((btnPushed >= 0 && btnPushed <= 9) || (btnPushed == "." && !this.state.calcVariable.includes('.')))
-        {
-            if((this.state.calcVariable == '0' || this.state.calcVariable == '+' || this.state.calcVariable == '-' || this.state.calcVariable == 'x' || this.state.calcVariable == '/') && btnPushed == "."){
-                this.setState({calcVariable : "0.", mathStatement : this.state.mathStatement + "0."})
-            }
-            else if((this.state.calcVariable == '0' || this.state.calcVariable == '+' || this.state.calcVariable == '-' || this.state.calcVariable == 'x' || this.state.calcVariable == '/') && btnPushed != "."){
-                this.setState({calcVariable : btnPushed, mathStatement : this.state.mathStatement + btnPushed})
-            }
-            else{
-                this.setState({calcVariable : this.state.calcVariable + btnPushed, mathStatement : this.state.mathStatement + btnPushed})
-            }
+        else if(this.state.mathStatement.includes('=') && btnPushed >=0){
+            this.setState({calcVariable: btnPushed, mathStatement: btnPushed})
         }
-        else if(btnPushed == "+")
-        {
-            if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && this.state.calcVariable != '-'){
-                this.setState({calcVariable : '+', mathStatement : this.state.mathStatement + "+"})
+        else{
+            if(btnPushed == "AC"){
+                this.setState({calcVariable: '0', mathStatement: ''})
             }
-            else{
-                if(this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length) == '--'){
-                    this.setState({calcVariable : '+', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "+"})
+            else if((btnPushed >= 0 && btnPushed <= 9) || (btnPushed == "." && !this.state.calcVariable.includes('.')))
+            {
+                if((this.state.calcVariable == '0' || this.state.calcVariable == '+' || this.state.calcVariable == '-' || this.state.calcVariable == 'x' || this.state.calcVariable == '/') && btnPushed == "."){
+                    if(this.state.calcVariable != '0' || btnPushed != '.'){
+                        this.setState({calcVariable : "0.", mathStatement : this.state.mathStatement + "0."})
+                    }
+                    else{
+                        this.setState({calcVariable : "0.", mathStatement : this.state.mathStatement + "0."})
+                    }
+                }
+                else if((this.state.calcVariable == '0' || this.state.calcVariable == '+' || this.state.calcVariable == '-' || this.state.calcVariable == 'x' || this.state.calcVariable == '/') && btnPushed != "."){
+                    if(this.state.calcVariable != '0' || btnPushed != '0'){
+                        this.setState({calcVariable : btnPushed, mathStatement : this.state.mathStatement + btnPushed})
+                    }
                 }
                 else{
-                    let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
-
-                    if(last2 == '+-' || last2 == '*-' || last2 == '/-'){
+                    this.setState({calcVariable : this.state.calcVariable + btnPushed, mathStatement : this.state.mathStatement + btnPushed})
+                }
+            }
+            else if(btnPushed == "+")
+            {
+                if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && this.state.calcVariable != '-'){
+                    this.setState({calcVariable : '+', mathStatement : this.state.mathStatement + "+"})
+                }
+                else{
+                    if(this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length) == '--'){
                         this.setState({calcVariable : '+', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "+"})
                     }
                     else{
-                        this.setState({calcVariable : '+', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 1) + "+"})
+                        let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
+
+                        if(last2 == '+-' || last2 == '*-' || last2 == '/-'){
+                            this.setState({calcVariable : '+', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "+"})
+                        }
+                        else{
+                            this.setState({calcVariable : '+', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 1) + "+"})
+                        }
                     }
                 }
-            }
-            
-            if(this.state.mathStatement.indexOf('=') >= 0){
-                this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "+"})
-            }
+                
+                if(this.state.mathStatement.indexOf('=') >= 0){
+                    this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "+"})
+                }
 
-        }
-        else if(btnPushed == "-")
-        {
-            let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
-
-            if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && last2 != '--' && last2 != '*-' && last2 != '/-' && last2 != '+-'){
-                this.setState({calcVariable : '-', mathStatement : this.state.mathStatement + "-"})
             }
-            else{
-                last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
+            else if(btnPushed == "-")
+            {
+                let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
 
-                if(last2 != '--' && last2 != '*-' && last2 != '/-' && last2 != '+-'){
+                if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && last2 != '--' && last2 != '*-' && last2 != '/-' && last2 != '+-'){
                     this.setState({calcVariable : '-', mathStatement : this.state.mathStatement + "-"})
                 }
+                else{
+                    last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
+
+                    if(last2 != '--' && last2 != '*-' && last2 != '/-' && last2 != '+-'){
+                        this.setState({calcVariable : '-', mathStatement : this.state.mathStatement + "-"})
+                    }
+                }
+                if(this.state.mathStatement.indexOf('=') >= 0){
+                    this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "-"})
+                }
             }
-            if(this.state.mathStatement.indexOf('=') >= 0){
-                this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "-"})
-            }
-        }
-        else if(btnPushed == "x")
-        {
-            if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && this.state.calcVariable != '-'){
-                this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement + "*"})
-            }
-            else{
-                if(this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length) == '--'){
-                    this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "*"})
+            else if(btnPushed == "x")
+            {
+                if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && this.state.calcVariable != '-'){
+                    this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement + "*"})
                 }
                 else{
-                    let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
-
-                    if(last2 == '+-' || last2 == '*-' || last2 == '/-'){
+                    if(this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length) == '--'){
                         this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "*"})
                     }
                     else{
-                        this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 1) + "*"})
+                        let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
+
+                        if(last2 == '+-' || last2 == '*-' || last2 == '/-'){
+                            this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "*"})
+                        }
+                        else{
+                            this.setState({calcVariable : 'x', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 1) + "*"})
+                        }
                     }
                 }
+                if(this.state.mathStatement.indexOf('=') >= 0){
+                    this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "*"})
+                }
             }
-            if(this.state.mathStatement.indexOf('=') >= 0){
-                this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "*"})
-            }
-        }
-        else if(btnPushed == "/")
-        {
-            if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && this.state.calcVariable != '-'){
-                this.setState({calcVariable : '/', mathStatement : this.state.mathStatement + "/"})
-            }
-            else{
-                if(this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length) == '--'){
-                    this.setState({calcVariable : '/', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "/"})
+            else if(btnPushed == "/")
+            {
+                if(this.state.calcVariable != '+' && this.state.calcVariable != 'x' && this.state.calcVariable != '/' && this.state.calcVariable != '-'){
+                    this.setState({calcVariable : '/', mathStatement : this.state.mathStatement + "/"})
                 }
                 else{
-                    let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
-
-                    if(last2 == '+-' || last2 == '*-' || last2 == '/-'){
+                    if(this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length) == '--'){
                         this.setState({calcVariable : '/', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "/"})
                     }
                     else{
-                        this.setState({calcVariable : '/', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 1) + "/"})
+                        let last2 = this.state.mathStatement.substring(this.state.mathStatement.length - 2, this.state.mathStatement.length);
+
+                        if(last2 == '+-' || last2 == '*-' || last2 == '/-'){
+                            this.setState({calcVariable : '/', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 2) + "/"})
+                        }
+                        else{
+                            this.setState({calcVariable : '/', mathStatement : this.state.mathStatement.substring(0, this.state.mathStatement.length - 1) + "/"})
+                        }
                     }
                 }
+                if(this.state.mathStatement.indexOf('=') >= 0){
+                    this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "/"})
+                }
             }
-            if(this.state.mathStatement.indexOf('=') >= 0){
-                this.setState({mathStatement : this.state.mathStatement.substring(this.state.mathStatement.indexOf('=')+1, this.state.mathStatement.length) + "/"})
-            }
-        }
-        else if(btnPushed == "=")
-        {
-            let mathExp = this.state.mathStatement;
-            let finalExp = mathExp.replace('--', '+');
+            else if(btnPushed == "=")
+            {
+                let mathExp = this.state.mathStatement;
+                let finalExp = mathExp.replace('--', '+');
 
-            this.setState({calcVariable: eval(finalExp), mathStatement : this.state.mathStatement + "=" + eval(finalExp)})
+                this.setState({calcVariable: eval(finalExp), mathStatement : this.state.mathStatement + "=" + eval(finalExp)})
+            }
         }
     }
 
